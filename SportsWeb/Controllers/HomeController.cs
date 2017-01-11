427d3288b920d8a60840afa04a100a1c3b63211f
vinodmangalore <vinodmangalore@gmail.com>
2017-01-11 15:28:32 +0530

    Testing

--- SportsWeb/Controllers/HomeController.cs
+++ SportsWeb/Controllers/HomeController.cs
@@ -26,14 +26,12 @@ namespace SportsWeb.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View(); 
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
